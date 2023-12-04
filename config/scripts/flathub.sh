20000dbaf65d7123dabb762bf221893a6e51e7ec
alextrical <35117191+alextrical@users.noreply.github.com>
2023-12-04 14:06:10 +0000

    Testing Flathub Add

--- config/scripts/flathub.sh
+++ config/scripts/flathub.sh
@@ -6,7 +6,7 @@
 set -oue pipefail
 
 # #Change flatpak to use flathub
-flatpak remote-delete --system --force fedora
-flatpak remote-delete --user --force fedora
+# flatpak remote-delete --system --force fedora
+# flatpak remote-delete --user --force fedora
 # flatpak remove --system --noninteractive --all
-# flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
--- config/scripts/stlthumb.sh
+++ config/scripts/stlthumb.sh
@@ -6,5 +6,4 @@
 set -oue pipefail
 
 # Your code goes here.
-rpm-ostree install https://github.com/alextrical/stl-thumb/releases/download/release/stl-thumb-0.5.0-1.x86_64.rpm
-rpm-ostree install https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stl-thumb-kde-0.5.0-Linux.rpm
+rpm-ostree install https://github.com/alextrical/stl-thumb/releases/download/release/stl-thumb-0.5.0-1.x86_64.rpm https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stl-thumb-kde-0.5.0-Linux.rpm
