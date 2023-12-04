9b02613bf3e7d788568dfe53c7a9a91dd80c1555
alextrical <35117191+alextrical@users.noreply.github.com>
2023-12-04 14:00:11 +0000

    Add STLThumb

---
+++ config/scripts/stlthumb.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+rpm-ostree install https://github.com/alextrical/stl-thumb/releases/download/release/stl-thumb-0.5.0-1.x86_64.rpm
+rpm-ostree install https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stl-thumb-kde-0.5.0-Linux.rpm
