d21d804856f0fc46bf9a79ce4243113be6a115f3
alextrical <35117191+alextrical@users.noreply.github.com>
2023-12-04 14:10:12 +0000

    STLThumb install fix

--- config/scripts/stlthumb.sh
+++ config/scripts/stlthumb.sh
@@ -6,4 +6,5 @@
 set -oue pipefail
 
 # Your code goes here.
-rpm-ostree install https://github.com/alextrical/stl-thumb/releases/download/release/stl-thumb-0.5.0-1.x86_64.rpm https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stl-thumb-kde-0.5.0-Linux.rpm
+rpm-ostree install https://github.com/alextrical/stl-thumb/releases/download/release/stl-thumb-0.5.0-1.x86_64.rpm
+rpm-ostree install https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stl-thumb-kde-0.5.0-Linux.rpm
