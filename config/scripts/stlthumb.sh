fb2805117606c2ee4d859ebf6f7d5fdbcda89d96
alextrical <35117191+alextrical@users.noreply.github.com>
2023-12-05 14:18:12 +0000

    Mightyscape and Inkstitch

--- config/scripts/stlthumb.sh
+++ config/scripts/stlthumb.sh
@@ -11,4 +11,5 @@ rpm-ostree install /tmp/stl-thumb-0.5.0-1.x86_64.rpm
 rm /tmp/stl-thumb-0.5.0-1.x86_64.rpm
 #rpm2cpio https://github.com/alextrical/stl-thumb/releases/download/release/stl-thumb-0.5.0-1.x86_64.rpm | cpio -idmv
 wget https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stlthumbnail.so -P /usr/lib64/qt5/plugins
+chmod +x /usr/lib64/qt5/plugins/stlthumbnail.so
 wget https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stlthumbnail.desktop -P /usr/share/kservices5
