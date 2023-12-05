336642b8d10f9d36692069244d29c18974f0e419
alextrical <35117191+alextrical@users.noreply.github.com>
2023-12-05 09:06:24 +0000

    Edit folder

--- config/scripts/stlthumb.sh
+++ config/scripts/stlthumb.sh
@@ -6,6 +6,6 @@
 set -oue pipefail
 
 # Your code goes here.
-(cd /var/log && rpm2cpio https://github.com/alextrical/stl-thumb/releases/download/release/stl-thumb-0.5.0-1.x86_64.rpm | cpio -D / -idmv)
+rpm2cpio https://github.com/alextrical/stl-thumb/releases/download/release/stl-thumb-0.5.0-1.x86_64.rpm | cpio -D / -idmv
 wget https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stlthumbnail.so -P /usr/lib64/qt5/plugins
 wget https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stlthumbnail.desktop -P /usr/share/kservices5
