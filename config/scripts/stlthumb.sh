c5c6acda2d1909f1600b2dd55b8aa049372d316f
alextrical <35117191+alextrical@users.noreply.github.com>
2023-12-05 08:37:43 +0000

    Changed folder location

--- config/scripts/stlthumb.sh
+++ config/scripts/stlthumb.sh
@@ -6,6 +6,6 @@
 set -oue pipefail
 
 # Your code goes here.
-(cd config/files && rpm2cpio https://github.com/alextrical/stl-thumb/releases/download/release/stl-thumb-0.5.0-1.x86_64.rpm | cpio -idmv)
-wget https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stlthumbnail.so -P config/files/usr/lib64/qt5/plugins
-wget https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stlthumbnail.desktop -P config/files/usr/share/kservices5
+(cd ~/work/makerspace-kinoite/makerspace-kinoite/config/files && rpm2cpio https://github.com/alextrical/stl-thumb/releases/download/release/stl-thumb-0.5.0-1.x86_64.rpm | cpio -idmv)
+wget https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stlthumbnail.so -P ~/work/makerspace-kinoite/makerspace-kinoite/config/files/usr/lib64/qt5/plugins
+wget https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stlthumbnail.desktop -P ~/work/makerspace-kinoite/makerspace-kinoite/config/files/usr/share/kservices5
