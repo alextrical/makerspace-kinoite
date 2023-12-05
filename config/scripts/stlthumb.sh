2367415d1b9c9a2acc61471671d884f273639b20
alextrical <35117191+alextrical@users.noreply.github.com>
2023-12-05 08:21:59 +0000

    STLThumb Alter extraction

--- config/scripts/stlthumb.sh
+++ config/scripts/stlthumb.sh
@@ -6,6 +6,6 @@
 set -oue pipefail
 
 # Your code goes here.
-rpm2cpio https://github.com/alextrical/stl-thumb/releases/download/release/stl-thumb-0.5.0-1.x86_64.rpm | cpio -D /config/files -idmv
-wget https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stlthumbnail.so -P /config/files/usr/lib64/qt5/plugins/
-wget https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stlthumbnail.desktop -P /config/files/usr/share/kservices5/
+rpm2cpio https://github.com/alextrical/stl-thumb/releases/download/release/stl-thumb-0.5.0-1.x86_64.rpm | cpio -D ./config/files -idmv
+wget https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stlthumbnail.so -P ./config/files/usr/lib64/qt5/plugins/
+wget https://github.com/alextrical/stl-thumb-kde/releases/download/Release/stlthumbnail.desktop -P ./config/files/usr/share/kservices5/
