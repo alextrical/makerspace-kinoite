37d031f67068e7afb0bcb935a4c0e52aa841268e
alextrical <35117191+alextrical@users.noreply.github.com>
2023-12-05 20:44:08 +0000

    Test

---
+++ config/scripts/appimagelauncher.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# This script grabs the latest AppImageLauncher release
+curl -s https://api.github.com/repos/TheAssassin/AppImageLauncher/releases/latest \
+| grep "browser_download_url.*x86_64.rpm" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| xargs wget - -O /tmp/appimagelauncher.x86_64.rpm
+rpm-ostree install /tmp/appimagelauncher.x86_64.rpm
+rm /tmp/appimagelauncher.x86_64.rpm
