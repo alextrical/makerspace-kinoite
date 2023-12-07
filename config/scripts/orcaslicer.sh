5603f74f5c27f53d451b36ab4cc4e2bb9ce96bf4
alextrical <35117191+alextrical@users.noreply.github.com>
2023-12-07 20:21:36 +0000

    OrcaSlicer Debug

--- config/scripts/orcaslicer.sh
+++ config/scripts/orcaslicer.sh
@@ -19,12 +19,12 @@ chmod +x /tmp/OrcaSlicer/OrcaSlicer.AppImage
 
 #Extract and move to Usr folder
 (cd /tmp/OrcaSlicer && /tmp/OrcaSlicer/OrcaSlicer.AppImage --appimage-extract)
-yes | cp -rf /tmp/OrcaSlicer/squashfs-root/usr/share/* /usr/share
-yes | cp -rf /tmp/OrcaSlicer/squashfs-root/bin/* /usr/bin
-yes | cp -rf /tmp/OrcaSlicer/squashfs-root/resources/* /usr/resources
+# yes | cp -rf /tmp/OrcaSlicer/squashfs-root/usr/share/* /usr/share
+# yes | cp -rf /tmp/OrcaSlicer/squashfs-root/bin/* /usr/bin
+# yes | cp -rf /tmp/OrcaSlicer/squashfs-root/resources/* /usr/resources
 
-#Setup Desktop file
-mv /tmp/OrcaSlicer/squashfs-root/OrcaSlicer.desktop /usr/share/applications/OrcaSlicer.desktop
-sed -i 's@AppRun@/usr/bin/orca-slicer@g' /usr/share/applications/OrcaSlicer.desktop
-sed -i 's@Utility;@Graphics;3DGraphics;Engineering;@g' /usr/share/applications/OrcaSlicer.desktop
-echo "Keywords=3D;Printing;Slicer;slice;3D;printer;convert;gcode;stl;obj;amf;SLA" >> /usr/share/applications/OrcaSlicer.desktop
+# #Setup Desktop file
+# mv /tmp/OrcaSlicer/squashfs-root/OrcaSlicer.desktop /usr/share/applications/OrcaSlicer.desktop
+# sed -i 's@AppRun@/usr/bin/orca-slicer@g' /usr/share/applications/OrcaSlicer.desktop
+# sed -i 's@Utility;@Graphics;3DGraphics;Engineering;@g' /usr/share/applications/OrcaSlicer.desktop
+# echo "Keywords=3D;Printing;Slicer;slice;3D;printer;convert;gcode;stl;obj;amf;SLA" >> /usr/share/applications/OrcaSlicer.desktop
